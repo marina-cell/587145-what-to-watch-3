5a26fdb44a30eeb7ed15e111709f03bf59e447e5
marina_cell <melissa_nsk_@mail.ru>
2020-02-08 10:44:05 +0700

    Установила React и создала первые компоненты

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/app.jsx";
+
+const Movie = {
+  title: `The Grand Budapest Hotel`,
+  genre: `Drama`,
+  year: 2014
+};
+
+ReactDOM.render(
+    <App
+      title={Movie.title}
+      genre={Movie.genre}
+      year={Movie.year}
+    />,
+    document.querySelector(`#root`)
+);
